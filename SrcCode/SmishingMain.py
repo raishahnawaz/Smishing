7034e8ae2aa24b1c11dd2679383b4d66a9d6a77d
Rai Shahnawaz <raishahnawaz@live.com>
2018-06-02 02:00:38 +0500

    Started Project - With Complete Configurations

---
+++ SrcCode/SmishingMain.py
@@ -0,0 +1,42 @@
+from pyspark.ml import Pipeline
+from pyspark.ml.classification import LogisticRegression
+from pyspark.ml.evaluation import BinaryClassificationEvaluator
+from pyspark.ml.feature import HashingTF, Tokenizer
+from pyspark.ml.tuning import CrossValidator, ParamGridBuilder
+
+
+from pyspark import SparkContext, SparkConf,SQLContext
+from pyspark.sql import functions as F
+import sys
+from operator import add
+from pyspark.sql import SparkSession
+import re
+from pydoc import  help
+from io import StringIO
+
+from operator import is_not
+from functools import partial
+import importlib
+
+importlib.reload(sys)
+
+sys.getdefaultencoding()
+from os import environ
+
+
+if __name__ == '__main__':
+
+    print ("Hello");
+
+    print(environ['SPARK_HOME'])
+    print(environ['JAVA_HOME'])
+    try:
+        print(environ['PYSPARK_SUBMIT_ARGS'])
+    except:
+        print(
+            "no problem with PYSPARK_SUBMIT_ARGS")  # https://github.com/ContinuumIO/anaconda-issues/issues/1276#issuecomment-277355043
+
+
+    conf = SparkConf().setAppName("Smishing")
+    sc=SparkContext(conf=conf)
+    sqlContext=SQLContext(sparkContext=sc)
